c9f96b60fb205c4ad8f814a13d1eb8833a013f51
BaurMus <musilimov_b@mail.ru>
2023-08-10 18:15:03 +0600

    update 'Avatar' 'NoAvatar' logic

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -5,11 +5,14 @@ import { Avatar } from "@mui/material";
 export const UserInfo = ({ avatarUrl, fullName, additionalText }) => {
   return (
     <div className={styles.root}>
-      {/* {avatarUrl 
+      {avatarUrl 
         ? <img className={styles.avatar} src={avatarUrl} alt={fullName}/> 
-        : <Avatar className={styles.noAvatar}>B</Avatar>
-      } */}
-      <Avatar alt={fullName} src={avatarUrl} sx={{width:30,height:30,marginRight:1}}/>
+        : <Avatar 
+              alt={fullName} 
+              className={styles.noAvatar} 
+              sx={{width: 30,height: 30,marginRight:1.3}}
+          >{fullName[0]}</Avatar>
+      }
       <div className={styles.userDetails}>
         <span className={styles.userName}>{fullName}</span>
         <span className={styles.additional}>{additionalText}</span>
